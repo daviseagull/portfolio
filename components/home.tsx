a1f9d1504f58124c6d1a872bc9d6884c1d3462fc
Davi Seagull <davi.seagull@gmail.com>
2024-02-24 20:44:03 -0500

    chore: fix profile photo height

--- components/home.tsx
+++ components/home.tsx
@@ -8,22 +8,24 @@ export default function Home() {
   const t = useTranslations("Home");
   return (
     <div className="flex flex-col items-center gap-6 pt-6 h-[calc(100vh-40px)] ">
-      <div className="h-1/4 aspect-square rounded-full overflow-hidden ">
-        <Image priority src={profilePhoto} alt="Profile Photo" />
+      <div className="h-1/3 aspect-square">
+        <div className=" aspect-square rounded-full overflow-hidden ">
+          <Image priority src={profilePhoto} alt="Profile Photo" />
+        </div>
+        <div className="flex items-center justify-center">
+          <SocialMedia />
+        </div>
       </div>
 
-      <div className="flex flex-col gap-4 h-3/4 justify-evenly">
+      <div className="flex flex-col h-2/3 justify-evenly">
         <div className="text-4xl font-bold">
           <h1>{t("greeting")}</h1>
           <h1>{t("name")}</h1>
         </div>
-        <h2 className="font-bold">{t("job-title")}</h2>
+        <h2 className="text-xl font-bold">{t("job-title")}</h2>
         <p className="font-light text-justify">{t("description")}</p>
         <p className="font-light text-justify">{t("second-description")}</p>
         <CallToAction />
-        <div className="flex items-center justify-center">
-          <SocialMedia />
-        </div>
       </div>
     </div>
   );
